5423680eb44b9e25130287db2524f41ca85d0294
brunomrlima <bruno.mrlima@gmail.com>
2020-06-13 19:44:54 -0400

    adding title to pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  private
+
+    def page_title(curr_page = '')
+      base_title = 'The Passage Shift'
+      if curr_page.empty?
+        base_title
+      else
+        curr_page + ' | ' + base_title
+      end
+    end
 end
