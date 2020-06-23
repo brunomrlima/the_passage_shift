dc346e574d990960040b88d6755b923b4b4783d9
brunomrlima <bruno.mrlima@gmail.com>
2020-06-22 22:50:55 -0400

    adding time range

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -4,5 +4,7 @@ class AvailabilitiesController < ApplicationController
 
   def new
     @days = Date::DAYNAMES
+    @time = []
+    (0..24).step(1) {|x| @time << "#{format('%02d', x)}:00"}
   end
 end
