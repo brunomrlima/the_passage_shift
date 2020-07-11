7a1880f7254f14988941600bd539791e9eb616a4
brunomrlima <bruno.mrlima@gmail.com>
2020-07-11 13:50:16 -0400

    availability header and root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   authenticated :user do
     devise_scope :user do
-      root to: 'devise/registrations#edit', as: :authenticated_root
+      root to: 'availabilities#index', as: :authenticated_root
     end
   end
   devise_scope :user do
