5259e7536a3787f8bcb37548bb8f454a77430bdb
brunomrlima <bruno.mrlima@gmail.com>
2020-07-10 23:20:11 -0400

    availabilities

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -3,7 +3,8 @@ class AvailabilitiesController < ApplicationController
   end
 
   def new
-    @days = Date::DAYNAMES
-    @time_list = (7..22).inject([]) {|arr, n| arr << "#{format('%02d', n)}:00"}
+    @days ||= Date::DAYNAMES
+    @time_list ||= (7..22).inject([]) {|arr, n| arr << "#{format('%02d', n)}:00"}
+    @availabilities ||= (0..6).inject([]) {|arr, n| arr << Availability.new(day: @days[n])}
   end
 end
