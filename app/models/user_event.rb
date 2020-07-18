21f732041ae06f630ab724e78f3528518cf2431e
brunomrlima <bruno.mrlima@gmail.com>
2020-07-18 16:27:14 -0400

    validate uniqueness work event and user id

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,4 +1,5 @@
 class UserEvent < ApplicationRecord
   belongs_to :user
   belongs_to :work_event
+  validates :user_id, uniqueness: { scope: :work_event_id }
 end
--- spec/models/user_event_spec.rb
+++ spec/models/user_event_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe UserEvent, type: :model do
       expect(build(:user_event)).to be_valid
     end
   end
+
+  describe 'Association' do
+    context 'belongs_to' do
+      it { should belong_to(:user) }
+      it { should belong_to(:work_event) }
+    end
+  end
 end
