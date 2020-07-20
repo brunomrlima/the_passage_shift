9c279bd3c7bd5d84ba98b74d7fbf6b9dd5a461c5
brunomrlima <bruno.mrlima@gmail.com>
2020-07-20 15:22:31 -0400

    joined work event

---
+++ app/mailers/sender_mailer.rb
@@ -0,0 +1,8 @@
+class SenderMailer < ApplicationMailer
+  def joined_work_event(user_id, work_event_id)
+    @user = User.find(user_id)
+    @work_event = WorkEvent.find(work_event_id)
+    start_time = @work_event.start_time.strftime("%a %^b-%d @ %H:%M")
+    mail(to: @user.email, subject: "#{start_time} | #{@work_event.title}")
+  end
+end
