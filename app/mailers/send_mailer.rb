852a40e08e049b8ea570fcd25466affa2c8eb087
brunomrlima <bruno.mrlima@gmail.com>
2020-07-21 15:54:30 -0400

    email when you are removed from work event

--- app/mailers/send_mailer.rb
+++ app/mailers/send_mailer.rb
@@ -7,4 +7,11 @@ class SendMailer < ApplicationMailer
     start_time = @work_event.start_time.strftime("%a %^b-%d @ %H:%M")
     mail(to: @user.email, subject: "#{start_time} | #{@work_event.title}")
   end
+
+  def removed_work_event(user_id, work_event_id)
+    @user = User.find(user_id)
+    @work_event = WorkEvent.find(work_event_id)
+    start_time = @work_event.start_time.strftime("%a %^b-%d @ %H:%M")
+    mail(to: @user.email, subject: "[REMOVED] #{start_time} | #{@work_event.title}")
+  end
 end
