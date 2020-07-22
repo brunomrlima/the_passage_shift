f745cb38f8a4eb1d69245d6c5abc8bf896ef973c
brunomrlima <bruno.mrlima@gmail.com>
2020-07-22 19:56:52 -0400

    adding default values with js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("packs/availabilities")
+require("packs/work_events")
 
 import "bootstrap"
 import "../stylesheets/application"
