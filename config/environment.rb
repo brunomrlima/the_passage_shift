b30325e19dedb86551f316f7a66d5dd8f658bb6b
brunomrlima <bruno.mrlima@gmail.com>
2020-12-02 13:32:13 -0500

    sendgrid api key

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :user_name => Rails.application.credentials.sendgrid_username,
-    :password => Rails.application.credentials.sendgrid_password,
+    :password => Rails.application.credentials.sendgrid_api_key,
     :domain => 'the-passage-shift.herokuapp.com/',
     :address => 'smtp.sendgrid.net',
     :port => 587,
